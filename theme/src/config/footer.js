eb7176e02b7a7199fe595c9bc363872cbb714c2f
Anirban Sikdar <anirban9293@gmail.com>
2022-11-20 18:01:22 +0530

    begining changes

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -1,29 +1,34 @@
 export const footerSocials = [
   {
     name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
+    url: 'https://twitter.com/anirban9293',
     icon: "mdi:twitter",
   },
   {
-    name: 'Youtube',
-    url: 'https://youtube.com/c/LittleSticks',
-    icon: "mdi:youtube",
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/anirban-sikdar-1a11161b9/',
+    icon: "mdi:linkedin",
   },
   {
     name: 'Github',
-    url: 'https://github.com/littlesticks',
+    url: 'https://github.com/anirban-1009',
     icon: "mdi:github",
   },
   {
     name: 'Discord',
-    url: 'https://littlesticks.dev/discord',
+    url: 'https://discordapp.com/users/732131254299787294',
     icon: "ic:baseline-discord",
+  },
+  {
+  name: 'Email',
+  url: 'mailto:anirban-1009@riseup.net',
+  icon: "ic:email",
   },
 ]
 
 export const footerLists = [
 	{
-		title: 'Company',
+		title: 'The Stuff',
 		items: [
 			{
 				title: 'About',
--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `IEEE-HITAM | A site for all you bright minds to know more about amazing stuff hitamites being upto.`,
-	description: `A simple, clean, and modern site by IEEE-HITAM`,
+	title: `Anirban.io | A site for all you bright minds to know more about amazing stuff hitamites being upto.`,
+	description: `A simple, clean, and modern site by Anirban Sikdar`,
 	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `IEEE`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `Anrban Sikdar`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
